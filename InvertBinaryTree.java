024959f212645fddb2546f152cbef48b0b0f7e1b
giahaocbl <giahaocbl@gmail.com>
2022-05-03 06:23:29 +0700

    Add InvertBinaryTree

--- InvertBinaryTree.java
+++ InvertBinaryTree.java
@@ -2,7 +2,7 @@
 public class InvertBinaryTree {
     public TreeNode invertTree(TreeNode root) {
         if (root == null)
-            return root;
+            return null;
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
